90a5b613dd72af4b584c776cb4e897728e29c4c8
Dom Falcone <dom.falcone.official@gmail.com>
2025-12-03 14:25:59 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -546,7 +546,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (footerContainer) {
         const versionDiv = document.createElement('div');
         versionDiv.className = 'site-version';
-        versionDiv.textContent = 'Версия сайта: 1.3.4. Идет разработка.';
+        versionDiv.textContent = 'Версия сайта: 1.3.5. Идет разработка.';
         footerContainer.appendChild(versionDiv);
     }
 });
